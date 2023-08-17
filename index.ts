a516594190a10dfba9042389791eab18e45cb31c
UTKARSH AGRAWAL <71021450+ishu-agrawal@users.noreply.github.com>
2023-08-17 16:28:12 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,28 +1,42 @@
 import express from 'express';
 import cors from 'cors';
+import { v4 as uuidv4 } from 'uuid'; // Import UUID generator
 
-// Initialize the express engine
 const app: express.Application = express();
-app.use(cors())
-// Take a port 3000 for running server.
+app.use(cors());
+app.use(express.json());
+
 const port: number = 3001;
 
+interface Employee {
+  Employee_ID: string;
+  Employee_Name: string;
+}
 
-let data =[
-    {Employee_ID : '1234', Employee_Name : 'ak'},
-    {Employee_ID : '124', Employee_Name : 'ak'},
-    {Employee_ID : '134', Employee_Name : 'ak'}
-    ]
+let data: Employee[] = [
+  { Employee_ID: uuidv4(), Employee_Name: 'ak' },
+  { Employee_ID: uuidv4(), Employee_Name: 'ak' },
+  { Employee_ID: uuidv4(), Employee_Name: 'ak' },
+];
 
-// Handling '/' Request
 app.get('/', (_req, _res) => {
-	_res.json(data);
+  _res.json(data);
 });
 
+app.put('/edit/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
 
+  const index = data.findIndex((item) => item.Employee_ID === id);
 
-// Server setup
+  if (index !== -1) {
+    data[index].Employee_Name = name;
+    res.json(data);
+  } else {
+    res.status(404).json({ message: 'Item not found' });
+  }
+});
+
 app.listen(port, () => {
-	console.log(`TypeScript with Express
-		http://localhost:${port}/`);
+  console.log(`TypeScript with Express: http://localhost:${port}/`);
 });
